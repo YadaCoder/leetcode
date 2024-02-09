12625d5f4e2e4d41339d6c9b8c1beed7bd2d0c43
YadaCoder <yadacoderr@gmail.com>
2024-02-10 00:24:15 +0300

    Power of Two

---
+++ Longest_Common_Prefix/script.ts
@@ -0,0 +1,13 @@
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0) return "";
+
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (i >= strs[j].length || strs[j][i] !== strs[0][i]) {
+        return strs[0].substring(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+}
